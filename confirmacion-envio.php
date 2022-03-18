42eca3b97a99763db80b6efc479396f15a79f141
Leonel94aguirre <aguirreleonel194@gmail.com>
2022-03-17 21:17:24 -0300

    cambios

---
+++ confirmacion-envio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body id="gracias">
+    <?php include_once "header.php" ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 py-5">
+                <h1>Gracias por contactarnos</h1>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
--- contacto.php
+++ contacto.php
@@ -2,6 +2,37 @@
 
 $pg = "contacto";
 
+if ($_POST) {
+    $nombre = $_POST["txtNombre"];
+    $correo = $_POST["txtCorreo"];
+    $telefono = $_POST["txtTelefono"];
+    $mensaje = $_POST["txtMensaje"];
+}
+
+// Varios destinatarios
+$para = "aguirreleonel194@gmail.com";
+$título = 'Recibiste un mensaje desde tu Web';
+
+// mensaje
+$cuerpo = "
+Nombre: $nombre <br>
+Correo: $correo <br>
+Telefono: $telefono <br>
+Mensaje: $mensaje
+";
+
+// Para enviar un correo HTML, debe establecerse la cabecera Content-type
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Cabeceras adicionales
+$cabeceras .= 'To: aguirreleonel194@gmail.com' . "\r\n";
+$cabeceras .= 'From: contacto@aguirre.com.ar' . "\r\n";
+
+// Enviarlo
+mail($para, $título, $cuerpo, $cabeceras);
+header("Location: confirmacion-envio.php");
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -14,9 +45,7 @@ $pg = "contacto";
     <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 
@@ -35,24 +64,19 @@ $pg = "contacto";
             <div class="col-12 col-sm-6">
                 <form action="" method="POST">
                     <div class="pb-3">
-                        <input type="text" name="txtNombre" id="txtNombre" placeholder="Nombre"
-                            class="form-control shadow">
+                        <input type="text" name="txtNombre" id="txtNombre" placeholder="Nombre" class="form-control shadow">
                     </div>
                     <div class="pb-3">
-                        <input type="email" name="txtCorreo" id="txtCorreo" placeholder="Correo"
-                            class="form-control shadow">
+                        <input type="email" name="txtCorreo" id="txtCorreo" placeholder="Correo" class="form-control shadow">
                     </div>
                     <div class="pb-3">
-                        <input type="tel" name="txtTelefono" id="txtTelefono" placeholder="Telefono/Whatsapp"
-                            class="form-control shadow">
+                        <input type="tel" name="txtTelefono" id="txtTelefono" placeholder="Telefono/Whatsapp" class="form-control shadow">
                     </div>
                     <div class="pb-3">
-                        <textarea name="txtMensaje" id="txtMensaje" placeholder="Escribe aqui el mensaje"
-                            class="form-control shadow"></textarea>
+                        <textarea name="txtMensaje" id="txtMensaje" placeholder="Escribe aqui el mensaje" class="form-control shadow"></textarea>
                     </div>
                     <div class="pb-3 float-end float-sm-start">
-                        <button type="submit" id="btnEnviar" name="btnEnviar"
-                            class="btn btn-blanco px-4">ENVIAR</button>
+                        <button type="submit" id="btnEnviar" name="btnEnviar" class="btn btn-blanco px-4">ENVIAR</button>
                     </div>
                 </form>
             </div>
@@ -62,14 +86,11 @@ $pg = "contacto";
         <div class="row">
             <div class="col-12 col-sm-3 text-center text-sm-start">
                 <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github"></i>
-                    <a href="https://www.linkedin.com" target="_blank" title="Linkedin"><i
-                            class="fab fa-linkedin-in"></i></a>
+                    <a href="https://www.linkedin.com" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a>
             </div>
-            <div class="col-12 col-sm-3 text-center text-sm-start">Sponsor <a href="http://depcsuite.com.ar"
-                    target="_blank">DePC Suite</a>
+            <div class="col-12 col-sm-3 text-center text-sm-start">Sponsor <a href="http://depcsuite.com.ar" target="_blank">DePC Suite</a>
             </div>
-            <div class="col-12 col-sm-3 text-center text-sm-start"><a
-                    href="to:aguirreleonel194@gmail.com">aguirreleonel194@gmail.com</a></div>
+            <div class="col-12 col-sm-3 text-center text-sm-start"><a href="to:aguirreleonel194@gmail.com">aguirreleonel194@gmail.com</a></div>
         </div>
         <div class="bloque-whatsapp">
             <a href="https://web.whatsapp.com/" target="_blank"><i class="fab fa-whatsapp pb-4 pt-3 px-3"></i></a>
@@ -77,4 +98,5 @@ $pg = "contacto";
     </footer>
 </body>
 <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 </html>
